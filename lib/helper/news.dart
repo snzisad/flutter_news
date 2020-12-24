6a8ab570d15935bd24715dd188b5e79c4d6e7c35
snzisad <snzisad@gmail.com>
2020-12-24 12:06:23 +0600

    post list

---
+++ lib/helper/news.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:news_portal/helper/data.dart';
+import 'package:news_portal/models/article_models.dart';
+import 'package:http/http.dart' as http;
+
+class News{
+  List<ArticleModel> news = [];
+
+  Future<void> getNews() async{
+    String url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey='+Apikey;
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if(jsonData['status'] == 'ok'){
+      jsonData["articles"].forEach((element){
+        if(element["urlToImage"] != null && element["description"] != null){
+          ArticleModel articleModel = ArticleModel(
+              element["author"], element["title"], element["description"], element["url"],
+              element["urlToImage"], element["content"], element["publishedAt"]
+          );
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_portal/helper/data.dart';
+import 'package:news_portal/helper/news.dart';
+import 'package:news_portal/models/article_models.dart';
 import 'package:news_portal/models/category_model.dart';
 import 'dart:developer' as developer;
 
@@ -10,12 +13,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<CategoryModel> categories;
+  List<CategoryModel> categories = new List();
+  List<ArticleModel> articles = new List();
+  bool isLoadingData = true;
 
   @override
   void initState() {
     super.initState();
     categories = getCategories();
+    getNews();
+  }
+
+  getNews() async{
+    var newsClass = News();
+    await newsClass.getNews();
+    articles = newsClass.news;
+    setState(() {
+      isLoadingData = false;
+    });
   }
 
   @override
@@ -33,25 +48,48 @@ class _HomeState extends State<Home> {
         ),
         elevation: 0.0,
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 80,
-              padding: EdgeInsets.symmetric(horizontal: 5),
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                shrinkWrap: true,
-                itemCount: categories.length,
-                itemBuilder: (context, index){
-                  return  CategoryTile(
-                    title: categories[index].catName,
-                    imgUrl: categories[index].imgURL,
-                  );
-                },
+      body: isLoadingData? Center(
+        child: Container(
+          child: CircularProgressIndicator(),
+        ),
+      ) : SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Container(child: Column(
+            children: <Widget>[
+              // categories
+              Container(
+                height: 80,
+                margin: EdgeInsets.only(bottom: 10),
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index){
+                    return  CategoryTile(
+                      title: categories[index].catName,
+                      imgUrl: categories[index].imgURL,
+                    );
+                  },
+                ),
               ),
-            )
-          ],
+
+              // Posts
+              Container(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  itemCount: articles.length,
+                  itemBuilder: (context, index){
+                    return  PostTile(
+                      title: articles[index].title,
+                      imgURL: articles[index].urlToImage,
+                      desc: articles[index].description,
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -78,7 +116,7 @@ class CategoryTile extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
           child: Stack(
             children: <Widget>[
-              Image.network(imgUrl, height: 80, fit: BoxFit.cover,),
+              CachedNetworkImage(imageUrl: imgUrl, height: 80, fit: BoxFit.cover,),
               Container(
                 alignment: Alignment.center,
                 color: Colors.black26,
@@ -97,5 +135,38 @@ class CategoryTile extends StatelessWidget {
       ),
     );
   }
+}
+
+class PostTile extends StatelessWidget{
+  final String imgURL, title, desc;
+  PostTile({this.imgURL, this.title, this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(7),
+              child: Image.network(imgURL)
+          ),
+          SizedBox(height: 3,),
+          Text(title, style: TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.w700,
+            color: Colors.black87
+          ),),
+          SizedBox(height: 5,),
+          Text(desc, style: TextStyle(
+              fontSize: 13,
+              color: Colors.black54
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
 }
 
